ca09206fee4e2ca32826129d150d5cb843397ab1
Alexis Javier Levano Saire <levanosaire@gmail.com>
2023-12-06 13:01:02 -0500

    Creating Variables

--- src/main/TrabajoFinal.java
+++ src/main/TrabajoFinal.java
@@ -1,9 +1,23 @@
 package main;
 
+import java.util.Scanner;
+
 public class TrabajoFinal {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        // Obtener la cantidad de estudiantes
+        System.out.print("Ingrese la cantidad de estudiantes: ");
+        int cantidadEstudiantes = scanner.nextInt();
+
+        // Arreglos para almacenar datos de estudiantes
+        String[] nombres = new String[cantidadEstudiantes];
+        String[] apellidos = new String[cantidadEstudiantes];
+        String[] identificadores = new String[cantidadEstudiantes];
+        String[] detallesRelevantes = new String[cantidadEstudiantes];
+        double[][] notas = new double[cantidadEstudiantes][3]; // Tres evaluaciones por estudiante
+        double[] pesos = new double[3]; // Pesos para cada evaluación
     }
 
 }
